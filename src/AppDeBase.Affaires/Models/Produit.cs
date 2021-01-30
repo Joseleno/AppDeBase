227bc4ab01f16a0e12ec2f9c4aec2c852385cbc5
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2021-01-30 14:11:36 -0500

    Création des modèles

---
+++ src/AppDeBase.Affaires/Models/Produit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AppDeBase.Affaires.Models
+{
+    public class Produit : Entity
+    {
+        public Guid FournisseurId { get; set; }
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DateInscription { get; set; }
+        public bool Active { get; set; }
+
+        public Fournisseur Fournisseur { get; set; }
+    }
+}
