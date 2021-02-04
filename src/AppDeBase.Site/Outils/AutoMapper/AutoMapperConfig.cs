ea30056de2d63a6b53bb7c44561677eb4b849612
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2021-02-03 21:14:26 -0500

    La création de ViewModels et la configuration d'AutoMapper

---
+++ src/AppDeBase.Site/Outils/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AppDeBase.Affaires.Models;
+using AppDeBase.Site.ViewModels;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDeBase.Site.Outils.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Adresse, AdresseViewModel>().ReverseMap();
+            CreateMap<Fournisseur, FournisseurViewModel>().ReverseMap();
+            CreateMap<Produit, ProduitViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/AppDeBase.Site/ViewModels/AdresseViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppDeBase.Site.ViewModels
+{
+    public class AdresseViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [MaxLength(6)]
+        [MinLength(4)]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [StringLength(200, ErrorMessage = "Le champ {0} doit comporter entre {2} et {1} caractères", MinimumLength = 3)]
+        public string Rue { get; set; }
+
+        public string Complement { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [StringLength(6, ErrorMessage = "Le champ {0} doit comporter {1} caractères", MinimumLength = 6)]
+        public string CodePostal { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [StringLength(100, ErrorMessage = "Le champ {0} doit comporter entre {2} et {1} caractères", MinimumLength = 3)]
+        public string Ville { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis")]
+        [StringLength(50, ErrorMessage = "Le champ {0} doit comporter entre {2} et {1} caractères", MinimumLength = 3)]
+        public string Province { get; set; }
+
+        [HiddenInput]
+        public Guid FournisseurId { get; set; }
+    }
+}
